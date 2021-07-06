4658e7af8359e579f8442e119f4bf96f0c7f07e8
jamesgildea92@protonmail.com <jamesgildea92@protonmail.com>
2021-07-06 13:30:53 +0100

    Attempt 1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    first = int(input('Give the first number:'))
+    second = int(input('Give the second number:'))
 
+    final = first * second
+
+    print(str(first) + ' + ' + str(second) + ' = ' + str(final))
 if __name__ == '__main__':
     main()
